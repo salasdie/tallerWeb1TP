0d0187576cc4f76cd2ba16c3733f890ba5574c11
martinmen <martinhmsuni@gmail.com>
2019-10-24 19:19:57 -0300

    add de archivos

--- proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorUsuario.java
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorUsuario.java
@@ -1,4 +1,32 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import ar.edu.unlam.tallerweb1.servicios.ServicioLogin;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.inject.Inject;
+
+@Controller
 public class ControladorUsuario {
+
+
+    @Inject
+    private ServicioLogin servicioLogin;
+
+    @RequestMapping("/perfilUsuario")
+    public ModelAndView irPerfilUsuario() {
+
+        ModelMap modelo = new ModelMap();
+
+        Usuario pergilUsuario = new Usuario();
+        modelo.put("usuario", pergilUsuario);
+
+        return new ModelAndView("perfilUsuario", modelo);
+    }
+
+
 }
